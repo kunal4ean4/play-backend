ad79b45b8941bc3ef199f5e232db7b3e2e500159
Kunal Kumar <kunal.kumar@publicisproduction.com>
2026-05-09 00:00:18 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
@@ -12,4 +11,14 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+// Routes import
+import userRouter from './routes/user.routes.js'
+//Routes decleration as per the industry standard
+// app.js: routes hit -> hit method in controller controller
+app.use("/api/v1/users",userRouter) //http://localhost:8000/users/register
+
+
+
 export default app;
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,12 @@
-const asyncHandler=(requestHandler)=>{
-    (req,res,next)=>{
-        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
-    }
-}
+/*
+This asyncHandler utility is commonly written in the utils folder
+ of a Node.js + Express backend to avoid repeatedly writing try-catch
+blocks for every async API controller.
+*/
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
-export {asyncHandler}
+export { asyncHandler };
