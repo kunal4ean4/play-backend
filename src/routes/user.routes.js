e993a2cdb8de6c102026b0260d4218ec2247a273
Kunal Kumar <kunal.kumar@publicisproduction.com>
2026-05-16 07:43:16 +0530

    routes updated

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,19 +1,35 @@
 import { Router } from "express";
-import {registerUser} from "../controllers/user.controller.js"
-import {upload} from "../middlewares/multer.middleware.js"
-const router=Router()
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        },
-        {
-            name: "coverImage",
-            maxCount:1
-        }
-    ])
-    ,registerUser)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser,
+);
+
+router.route("/login").post(loginUser);
+
+//Secured routes
+// Note: Here verifyJWT is middleware and then logoutUser will be hit
+
+router.route("/logout").post(verifyJWT, logoutUser);
+
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router;
